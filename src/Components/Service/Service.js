e9d834e6e963cddcf43d1a70c3a1cad915d104be
dev-rahim <rahimuddin5915@gmail.com>
2022-12-10 23:14:15 +0600

    addded hover style

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Service.css'
 
 const Service = (props) => {
     const { title, id, image, description } = props.service;
     return (
         <div className='col-lg-4'>
-            <div className="p-2 m-2 rounded ">
+            <div className="p-3 m-2 rounded service-cart">
                 <h2 className='d-flex justify-content-between'><span className='text-start'>{title}</span><span className='text-end'>{id}</span></h2>
-
                 <img className='img-fluid rounded' src={image} alt="" srcset="" />
                 <p className='text-start'>{description.slice(0, 100)}</p>
                 <Link className='text-start nav-link' to=''>
