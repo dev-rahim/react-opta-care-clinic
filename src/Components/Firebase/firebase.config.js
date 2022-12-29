44b296c8b6c065f6001a74fec1ed499976be300e
dev-rahim <rahimuddin5915@gmail.com>
2022-12-29 17:00:37 +0600

    secqure firebase config

--- src/Components/Firebase/firebase.config.js
+++ src/Components/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD-qBL9OIndb57fnISvIex2qK4593XoOpw",
-    authDomain: "react-opta-care-clinic.firebaseapp.com",
-    projectId: "react-opta-care-clinic",
-    storageBucket: "react-opta-care-clinic.appspot.com",
-    messagingSenderId: "723287662502",
-    appId: "1:723287662502:web:2f6057b64acc6619673967"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 export default firebaseConfig;
