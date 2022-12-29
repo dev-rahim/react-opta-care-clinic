0d45a4896fa53bc566003c93521e2c85157069e0
dev-rahim <rahimuddin5915@gmail.com>
2022-12-29 20:03:58 +0600

    added notFound page illustration

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './NotFound.css'
 const NotFound = () => {
     return (
-        <div>
-            <h2>Page not found</h2>
+        <div className='page-not-found text-center mx-auto  container  mx-2'>
+            <div className="">
+                <Link className='back-to-home-btn' to={'/home'}>
+                    <button className='btn btn-secondary'>Back to Home</button>
+                </Link>
+            </div>
         </div>
     );
 };
